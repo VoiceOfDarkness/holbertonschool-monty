178b706ef70d7c873da1598940a7e65670535b17
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-01-09 03:59:22 -0800

    hot fix

--- swap.c
+++ swap.c
@@ -7,15 +7,20 @@
  */
 void _swap(stack_t **stack, unsigned int line)
 {
-	int temp;
+	stack_t *current;
+	stack_t *node;
 
+	current = *stack;
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line);
 		exit(EXIT_FAILURE);
 	}
 
-	temp = (*stack)->n;
-	(*stack)->n = (*stack)->next->n;
-	(*stack)->next->n = temp;
+	node = current->next;
+	current->next = node->next;
+	node->next = *stack;
+	current->prev = node;
+	node->prev = NULL;
+	*stack = node;
 }
