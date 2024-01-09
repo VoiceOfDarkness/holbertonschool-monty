3628b7a6d4f2f3a49c4981e9aec1b4ce77d63fbb
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2024-01-09 03:47:05 -0800

    Created pop and nop function

---
+++ nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * _nop - does not do anything
+ * @stack: stack pointer to head
+ * @line: number of line of code
+ */
+void _nop(stack_t **stack, unsigned int line)
+{
+	(void)stack;
+	(void)line;
+}
---
+++ pop.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+/**
+ * _pop - prints top of stack
+ * @stack: input stack
+ * @line: number of line
+ */
+void _pop(stack_t **stack, unsigned int line)
+{
+	stack_t *temp = *stack;
+
+	if (stack && *stack)
+	{
+		*stack = (*stack)->next;
+		(*stack)->prev = NULL;
+		free(temp);
+		return;
+	}
+	fprintf(stderr, "L%u: can't pop an empty stack\n", line);
+	exit(EXIT_FAILURE);
+}
