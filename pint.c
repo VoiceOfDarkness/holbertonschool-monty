38cc749cf472b645bcb222423903132ec418f768
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2024-01-09 03:00:06 -0800

    Fixed stderr bug in _pint

--- pint.c
+++ pint.c
@@ -11,6 +11,6 @@ void _pint(stack_t **stack, unsigned int line)
 		printf("%d\n", (*stack)->n);
 		return;
 	}
-	fprintf(stderr, "L%u: can't pint, stack empty", line);
+	fprintf(stderr, "L%u: can't pint, stack empty\n", line);
 	exit(EXIT_FAILURE);
 }
